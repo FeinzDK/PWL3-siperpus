5477c819e567125f096e2e9b0c23e6f4cccaeaec
andalasmaulanamalik <andalas205@gmail.com>
2024-11-23 09:18:13 +0700

    Pertemuan 7 - UpdateDelete

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Book;
 use App\Models\Bookshelf;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class BookController extends Controller
 {
@@ -23,7 +24,7 @@ class BookController extends Controller
         $validated = $request->validate([
             'title' => 'required|max:255',
             'author' => 'required|max:255',
-            'year' => 'required|min:1945|max:2077',
+            'year' => 'required|integer|min:1945|max:2077',
             'publisher' => 'required|max:255',
             'city' => 'required|max:50',
             'cover' => 'required',
@@ -48,6 +49,57 @@ class BookController extends Controller
                 'alert-type' => 'error'
             );
         }
+        return redirect()->route('book')->with($notification);
+    }
+    public function edit(string $id){
+        $data['book'] = Book::findOrFail($id);
+        $data['bookshelves'] = Bookshelf::pluck('name','id');
+        return view('books.edit',$data);
+    }
+    public function update(Request $request, string $id)
+    {
+        $book = Book::findOrFail($id);
+        $validated = $request->validate([
+            'title' => 'required|max:255',
+            'author' => 'required|max:255',
+            'year' => 'required|integer|min:1945|max:2077',
+            'publisher' => 'required|max:255',
+            'city' => 'required|max:50',
+            'cover' => 'required',
+            'bookshelf_id' => 'required|max:5',
+        ]);
+        if ($request->hasFile('cover')) {
+            if($book->cover != null){
+                Storage::delete('public/cover_buku'.$book->old_cover);
+            }
+            $path = $request->file('cover')->storeAs(
+                'public/cover_buku',
+                'cover_buku_' . time() . '.' . $request->file('cover')->extension()
+            );
+            $validated['cover'] = basename($path);
+        }
+        $book->update($validated);
+        if ($book) {
+            $notification = array(
+                'message' => 'Data buku berhasil disimpan',
+                'alert-type' => 'success'
+            );
+        } else {
+            $notification = array(
+                'message' => 'Data buku gagal disimpan',
+                'alert-type' => 'error'
+            );
+        }
+        return redirect()->route('book')->with($notification);
+    }
+    public function destroy(string $id){
+        $book = Book::findOrFail($id);
+        Storage::delete('public/cover_buku/'.$book->cover);
+        $book->delete();
+        $notification = array(
+            'message' => 'Data buku berhasil dihapus',
+            'alert-type' => 'success'
+        );
         return redirect()->route('book')->with($notification);
     }
 }
